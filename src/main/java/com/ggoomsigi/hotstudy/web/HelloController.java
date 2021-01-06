18d1d39e9400025e6b0b46a839790638298ef079
areum <areum9502@gmail.com>
2021-01-06 20:44:15 +0900

    init

--- src/main/java/com/ggoomsigi/hotstudy/web/HelloController.java
+++ src/main/java/com/ggoomsigi/hotstudy/web/HelloController.java
@@ -1,16 +1,21 @@
 package com.ggoomsigi.hotstudy.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     // 1. HTTP Method : GET, POST, PUT, DELETE etc ...
     // 2. URL Path : /hello, /hi, /login, /sign-up etc...
     // 3. Do Something
 
     @GetMapping("/hello")
-    public String hello() { return "hello"; }
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
 
     @GetMapping("/hello1")
     public String hello1() { return "hello1"; }
